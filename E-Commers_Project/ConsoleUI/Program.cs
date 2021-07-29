acc9047b6382300ff22e4b7a5bb6abc3a862c74a
iremaltnz <55363753+iremaltnz@users.noreply.github.com>
2021-07-29 19:08:56 +0300

    güncellenmiş

--- E-Commers_Project/ConsoleUI/Program.cs
+++ E-Commers_Project/ConsoleUI/Program.cs
@@ -8,43 +8,10 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductDalTest();
-            CategoryDalTest();
+            
 
         }
 
-        private static void CategoryDalTest()
-        {
-            NSPCategoryDal nSPCategoryDal = new NSPCategoryDal();
-            string query = "select*from categories";
-            DataSet dataSet = new DataSet();
-            dataSet = nSPCategoryDal.List(query);
-
-            for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
-            {
-                Console.Write(dataSet.Tables[0].Rows[i]["category_id"].ToString());
-
-                Console.Write(dataSet.Tables[0].Rows[i]["category_name"].ToString());
-                Console.WriteLine();
-
-            }
-        }
-
-        private static void ProductDalTest()
-        {
-            NSProductDal nSProductDal = new NSProductDal();
-            string query = "select*from products";
-            DataSet dataSet = new DataSet();
-            dataSet = nSProductDal.List(query);
-
-            for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
-            {
-                Console.Write(dataSet.Tables[0].Rows[i]["product_id"].ToString());
-
-                Console.Write(dataSet.Tables[0].Rows[i]["product_name"].ToString());
-                Console.WriteLine();
-
-            }
-        }
+       
     }
 }
