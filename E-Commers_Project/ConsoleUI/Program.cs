36a3e2727a4a8bfe6411a0c418b14bf3c2b7404c
Mehmet Karasu <mkarasu416@gmail.com>
2021-07-31 01:56:19 +0300

    Commit4

--- E-Commers_Project/ConsoleUI/Program.cs
+++ E-Commers_Project/ConsoleUI/Program.cs
@@ -26,17 +26,17 @@ namespace ConsoleUI
                 Console.Write(dataSet.Tables[0].Rows[i]["brand_id"].ToString());
 
                 Console.Write(dataSet.Tables[0].Rows[i]["brand_name"].ToString());
-           
+                Console.WriteLine();
 
-             }
+            }
 
         }
         private static void CategoryDalTest()
         {
-            NSPCategoryDal nSPCategoryDal = new NSPCategoryDal();
+            NSCategoryDal nSCategoryDal = new NSCategoryDal();
             string query = "select*from categories";
             DataSet dataSet = new DataSet();
-            dataSet = nSPCategoryDal.List(query);
+            dataSet = nSCategoryDal.List(query);
 
             for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
             {
