bf15c09c4befbba32a7f52527665b0a25405f53b
Mehmet Karasu <mkarasu416@gmail.com>
2021-07-31 20:13:20 +0300

    Commit5

---
+++ E-Commers_Project/Business/Abstract/IColorService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IColorService
+    {
+        List<Color> List();
+        void Add(Color color);
+        void Delete(Color color);
+        void Update(Color color);
+
+    }
+}
---
+++ E-Commers_Project/Business/Concrete/BrandManager.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+        DataSet dataSet;
+        string query;
+
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+        public void Add(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> List()
+        {
+            query = "select* from public.brands";
+            dataSet = _brandDal.List(query);
+            Brand brand = new Brand();
+            List<Brand> brands = new List<Brand>();
+
+
+            for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+            {
+                brand = new Brand();
+                
+                brand.BrandId = Convert.ToInt32(dataSet.Tables[0].Rows[i]["brand_id"]);
+             
+
+                brands.Add(brand);
+            }
+
+            return brands;
+        }
+
+        public void Update(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
