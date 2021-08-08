44a9d6d43042446eb15893c1b31c79ae67a2aaba
Mehmet Karasu <mkarasu416@gmail.com>
2021-08-08 19:40:23 +0300

    Unit Test - Brand,Color

---
+++ E-Commers_Project/UnitTest/BrandTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Business.Concrete;
+using Entities.Concrete;
+using DataAccess.Concrete.NpgSql;
+using NUnit.Framework.Internal;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class BrandTest
+    {
+        BrandManager brandManager = new BrandManager(new NSBrandDal());
+        Brand brand = new Brand() { brand_name = "Nike" };
+        [Test]
+        public void BrandList()
+        {
+            var result = brandManager.List();
+            Assert.AreEqual(result.Success, true, "Markalar Listelenemedi");
+        }
+
+        [Test]
+        public void BrandAdd()
+        {
+            var result = brandManager.Add(brand);
+            Assert.AreEqual(result.Success, true, "Markalar Eklenemedi");
+        }
+        [Test]
+        public void BrandUpdate()
+        {
+            var result = brandManager.Update(brand);
+            Assert.AreEqual(result.Success, true, "Markalar Güncellenemedi");
+        }
+        [Test]
+        /*
+        public void BrandDelete()
+        {
+            var result = brandManager.Delete(brand);
+            Assert.AreEqual(result.Success, true, "Markalar Silinemedi");
+        }
+        */
+
+    }
+}
